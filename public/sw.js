ccb4add56f519f3e94b2819dff5af2edf8c69d91
rjah-labs <robert.j.hilder@gmail.com>
2026-08-26 14:04:25 +1000

    Update public/sw.js for v0.8a

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,6 @@
-const CACHE="physique-v0.7.2";
+const CACHE="physique-v0.8.0";
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["./","./manifest.webmanifest"]))));
 self.addEventListener("activate",event=>event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()])));
 self.addEventListener("message",event=>{if(event.data?.type==="SKIP_WAITING")self.skipWaiting()});
+self.addEventListener("notificationclick",event=>{event.notification.close();event.waitUntil(clients.matchAll({type:"window",includeUncontrolled:true}).then(windows=>windows[0]?windows[0].focus():clients.openWindow("./")))});
 self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match("./"))))});
