8c165794bbf44e70a971e4c49b4c0f16cc5badd6
rjah-labs <robert.j.hilder@gmail.com>
2026-08-26 10:43:42 +1000

    Update public/sw.js for v0.7.1

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="physique-v0.7.0";
+const CACHE="physique-v0.7.1";
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(["./","./manifest.webmanifest"]))));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
 self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match("./"))))});
